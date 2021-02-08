deb379abb5faf1ec612f8529fd863075c8f9acbe
tharinduEranga <tharindueranga3@gmail.com>
2021-02-08 08:15:02 +0530

    added files to git

---
+++ src/main/java/com/icbt/ap/sales/SalesSystemServerApplication.java
@@ -0,0 +1,13 @@
+package com.icbt.ap.sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesSystemServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesSystemServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/icbt/ap/sales/SalesSystemServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icbt.ap.sales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesSystemServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
